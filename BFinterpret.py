aa80452ddcdea2bdd8831e735584c8a848076c70
User <Willspri@gmail.com>
2024-09-02 12:47:55 +1200

    instructions completed

--- BFinterpret.py
+++ BFinterpret.py
@@ -8,11 +8,48 @@ def BFintperpret(code, input_data=""):
     output = ""  
     loop_stack = []  
     
-    #instuction interpreter
+    #instuction interpreter for +,><[]-
+    while code_pointer < len(code):
+        command = code[code_pointer]
+        
+        if command == '>':
+            pointer += 1 
+        elif command == '<':
+            pointer -= 1  
+        elif command == '+':
+            tape[pointer] = (tape[pointer] + 1) % 256  
+        elif command == '-':
+            tape[pointer] = (tape[pointer] - 1) % 256  
+        elif command == '.':
+            output += chr(tape[pointer])  
+        elif command == ',':
+            if input_pointer < len(input_data):
+                tape[pointer] = ord(input_data[input_pointer])  
+                input_pointer += 1
+        elif command == '[':
+            if tape[pointer] == 0:
+                open_loops = 1
+                while open_loops > 0:
+                    code_pointer += 1
+                    if code[code_pointer] == '[':
+                        open_loops += 1
+                    elif code[code_pointer] == ']':
+                        open_loops -= 1
+            else:
+                loop_stack.append(code_pointer)  
+        elif command == ']':
+            if tape[pointer] != 0:
+                code_pointer = loop_stack[-1]  
+            else:
+                loop_stack.pop()  
+
+        code_pointer += 1  
+
+    return output
 
 
 
-bf_code = "" #edit here
+bf_code = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++." #edit here
 decoded = BFintperpret(bf_code)
 print(decoded)
     
